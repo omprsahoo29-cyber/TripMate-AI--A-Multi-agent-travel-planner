ac166c1a8f4166645cadab66f58ea39176638945
omprakash <omprsahoo29@gmail.com>
2026-09-24 18:57:02 +0530

    Add flight and tavily tools

---
+++ test.py
@@ -0,0 +1,6 @@
+from tools.tavily_tool import tavily_search
+from tools.flight_tool import search_flights
+
+# print(tavily_search("Best hotels in india?"))
+
+print(search_flights("Plan a 7 days Japan trip from Delhi"))
